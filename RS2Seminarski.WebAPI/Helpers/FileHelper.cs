2d84a650d423468e678bcc3283a54dc1351da925
Dino <elvedingran@gmail.com>
2022-08-07 16:45:36 +0200

    add image seed

---
+++ RS2Seminarski.WebAPI/Helpers/FileHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace RS2Seminarski.WebAPI.Helpers
+{
+    public class FileHelper
+    {
+        public static byte[] ReadFile(string sPath)
+        {
+            FileInfo fInfo = new FileInfo(sPath);
+            long numBytes = fInfo.Length;
+
+            FileStream fStream = new FileStream(sPath, FileMode.Open, FileAccess.Read);
+
+            BinaryReader bReader = new BinaryReader(fStream);
+
+            byte[] data = bReader.ReadBytes((int)numBytes);
+
+            return data;
+        }
+    }
+}
--- RS2Seminarski.WebAPI/Migrations/20220807143514_ImageMigration.Designer.cs
+++ RS2Seminarski.WebAPI/Migrations/20220807143514_ImageMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RS2Seminarski.WebAPI.Database;
 
@@ -11,9 +12,10 @@ using RS2Seminarski.WebAPI.Database;
 namespace RS2Seminarski.WebAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220807143514_ImageMigration")]
+    partial class ImageMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
